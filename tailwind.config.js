aefefe258869668465f35719b0a824fa7e76653b
hibahfathima15 <hibahfathima15@gmail.com>
2024-06-03 11:03:30 +0530

    added user Login Page,Signup page started, starting animation page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,57 @@
 
 const colors = require('tailwindcss/colors')
-export default  {
+export default {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+
+        SlidefromLeft: {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+        SlidefromRight: {
+          '0%': { transform: 'translateX(100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+        appear: {
+          '0%': {  opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        diagonalSlide: {
+          '0%': { transform: 'translateX(100%)',transform: 'translateY(100%)', opacity: '0' },
+          '10%': { transform: 'translateX(90%)',transform: 'translateY(90%)', opacity: '0' },
+          '20%': { transform: 'translateX(80%)',transform: 'translateY(80%)', opacity: '0' },
+          '30%': { transform: 'translateX(70%)',transform: 'translateY(70%)', opacity: '0' },
+          '100%': {transform: 'translateX(0)',transform: 'translateY(0)', opacity: '1' },
+        }
+
+      },
+      animation: {
+        "SlidefromLeft": 'SlidefromLeft 1s ease-in-out',
+        "SlidefromRight": 'SlidefromRight 1s ease-in-out',
+        "HeaderfromRight": 'SlidefromRight 5s ease-in-out',
+        "appear": 'appear 1s linear',
+        "diagonalSlide": 'diagonalSlide 1s ease-in-out',
+      },
+
+
+
+
+    },
     fontFamily: {
       Roboto: ['Roboto', 'sans-serif'],
       poppins: ['Poppins', 'sans-serif'],
       plusjakartasans: ['"Plus Jakarta Sans"', 'sans-serif'],
- 
+
     },
     colors: {
       ...colors,
       transparent: 'transparent',
       current: 'currentColor',
-            "MediumBlue":"#E7ECFF"
+      "MediumBlue": "#E7ECFF"
     },
   },
   plugins: [],

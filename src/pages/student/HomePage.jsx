bdbf831e5ebcb40e54671f28565567ca79c3b966
swarna856 <swarnaav82@gmail.com>
2024-06-05 12:41:06 +0530

    changes in animation page

--- src/pages/student/HomePage.jsx
+++ src/pages/student/HomePage.jsx
@@ -1,46 +1,46 @@
-import React, { useState } from 'react';
-import AdminNavbar from '../../components/admin/AdminNavbar';
+import React, { useState } from "react";
+import AdminNavbar from "../../components/admin/AdminNavbar";
 // import Sidebar from '../../components/admin/Sidebar';
 import { Route, Routes } from "react-router-dom";
-import ErrorPage from '../ErrorPage';
-import CourseStructure from '../../components/admin/CourseStructure';
-import TutorListing from '../../components/admin/TutorListing';
-import AddTutor from '../../components/admin/AddTutor';
-import StudentList from '../../components/admin/StudentList';
-import TutorDetails from '../../components/admin/TutorDetails';
-import DiagnosisTest from '../../components/admin/DiagnosisTest';
-import Addiagnosistest from '../../components/admin/Addiagnosistest';
-import Coursedetails from '../../components/admin/Coursedetails';
-import CourseList from '../../components/admin/CourseList';
-import Library from '../../components/admin/Library';
-import AddCourse from '../../components/admin/AddCourse';
-import StudentDetail from '../../components/admin/StudentDetail';
-import UserNavbar from '../../components/User/UserNavbar';
-import AnimationScreen from '../../components/User/AnimationScreen';
+import ErrorPage from "../ErrorPage";
+import CourseStructure from "../../components/admin/CourseStructure";
+import TutorListing from "../../components/admin/TutorListing";
+import AddTutor from "../../components/admin/AddTutor";
+import StudentList from "../../components/admin/StudentList";
+import TutorDetails from "../../components/admin/TutorDetails";
+import DiagnosisTest from "../../components/admin/DiagnosisTest";
+import Addiagnosistest from "../../components/admin/Addiagnosistest";
+import Coursedetails from "../../components/admin/Coursedetails";
+import CourseList from "../../components/admin/CourseList";
+import Library from "../../components/admin/Library";
+import AddCourse from "../../components/admin/AddCourse";
+import StudentDetail from "../../components/admin/StudentDetail";
+import UserNavbar from "../../components/User/UserNavbar";
+import AnimationScreen from "../../components/User/AnimationScreen";
 
 const Homepage = () => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-    };
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
-    return (
-        <div className=" h-screen overflow-hidden">
-       
-                <div className="sticky top-0 z-10">
-                    <UserNavbar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
-                </div>
+  return (
+    <div className=" h-screen w-full ">
+      <div className="sticky top-0 z-10">
+        <UserNavbar
+          toggleSidebar={toggleSidebar}
+          isSidebarOpen={isSidebarOpen}
+        />
+      </div>
 
-            <div className="flex flex-col flex-grow">
-             
+      <div className="flex flex-col w-full h-full flex-grow">
+        {/* Main Content */}
 
-                {/* Main Content */}
-
-                <div className="flex-grow overflow-auto ">
-                    <Routes>
-                        <Route path="/*" element={<AnimationScreen/>} />
-                        {/* <Route path="/courses/addcourse" element={<AddCourse />} />
+        <div className="flex-grow overflow-auto ">
+          <Routes>
+            <Route path="/*" element={<AnimationScreen />} />
+            {/* <Route path="/courses/addcourse" element={<AddCourse />} />
                         <Route path="/courses" element={<CourseList />} />
                         <Route path="/courses/:courseId" element={<Coursedetails />} />
                         <Route path="/coursestructure" element={<CourseStructure />} />
@@ -52,12 +52,12 @@ const Homepage = () => {
                         <Route path="/tutors/:tutorId" element={<TutorDetails />} />
                         <Route path="/tutors" element={<TutorListing />} />
                         <Route path="/library" element={<Library />} /> */}
-                        <Route path="*" element={<ErrorPage />} />
-                    </Routes>
-                </div>
-            </div>
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -1,24 +1,53 @@
-import React from 'react'
-import { Route, Routes, Navigate, BrowserRouter as Router } from "react-router-dom";
-import { useSelector } from 'react-redux';
-import Login from '../pages/student/Login';
-import Homepage from '../pages/student/HomePage';
-import Signup from '../components/User/Signup';
+import React from "react";
+import {
+  Route,
+  Routes,
+  Navigate,
+  BrowserRouter as Router,
+} from "react-router-dom";
+import { useSelector } from "react-redux";
+import Login from "../pages/student/Login";
+import Homepage from "../pages/student/HomePage";
+import Signup from "../components/User/Signup";
+import Animation_ReviewPage from "../components/User/Animation_ReviewPage/Animation_ReviewPage";
+import Animation_Courses from "../components/User/Animation_Courses/Animation_Courses";
+import Animation_Tutor from "../components/User/Animation_Tutor/Animation_Tutor";
+import Animation_Methodolgy from "../components/User/Animation_Methodology/Animation_Methodolgy";
 
 const UserRoutes = () => {
-    const user = useSelector((state) => state.token.user)
-console.log(user)
+  const user = useSelector((state) => state.token.user);
+  console.log(user);
   return (
     <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route
+        path="/login"
+        element={user ? <Navigate to="/home" /> : <Login />}
+      />
+      <Route
+        path="/Signup"
+        element={user ? <Navigate to="/home" /> : <Signup />}
+      />
+      <Route
+        path="/review"
+        element={user ? <Navigate to="/home" /> : <Animation_ReviewPage />}
+      />
+      <Route
+        path="/courses"
+        element={user ? <Navigate to="/home" /> : <Animation_Courses />}
+      />
+      <Route
+        path="/tutor"
+        element={user ? <Navigate to="/home " /> : <Animation_Tutor />}
+      />
+      <Route
+        path="/methodology"
+        element={user ? <Navigate to="/home " /> : <Animation_Methodolgy />}
+      />
 
-    <Route path="/" element={ <Homepage />} />
-    <Route path="/login" element={user ? <Navigate to="/home" /> : <Login />} />
-    <Route path="/Signup" element={user ? <Navigate to="/home" /> : <Signup />} />
-    {/* <Route path="/home/*" element={user ? <Homepage /> : <Navigate to="/admin" />} /> */}
+      {/* <Route path="/home/*" element={user ? <Homepage /> : <Navigate to="/admin" />} /> */}
+    </Routes>
+  );
+};
 
-
-</Routes>
-  )
-}
-
-export default UserRoutes
+export default UserRoutes;

f8b5731b714b8decc3ecf0882a889e06b2a79901
swarna856 <swarnaav82@gmail.com>
2024-06-05 14:44:10 +0530

    error

--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import {
   Route,
@@ -18,21 +17,6 @@ import Animation_Methodolgy from "../components/User/Animation_Methodology/Anima
 const UserRoutes = () => {
   const user = useSelector((state) => state.token.user);
   console.log(user);
-=======
-import React from 'react'
-import { Route, Routes, Navigate, BrowserRouter as Router } from "react-router-dom";
-import { useSelector } from 'react-redux';
-import Login from '../pages/student/Login';
-import Homepage from '../pages/student/HomePage';
-import Signup from '../components/User/Signup';
-import StudentHomepage from '../pages/student/StudentHomepage';
-
-const UserRoutes = () => {
-
-    const user = useSelector((state) => state.token.user)
-    console.log(user)
-
->>>>>>> c47d8d5d0a8990cd8b71e1bb3b527dc11be15b34
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
@@ -61,17 +45,9 @@ const UserRoutes = () => {
         element={user ? <Navigate to="/home " /> : <Animation_Methodolgy />}
       />
 
-<<<<<<< HEAD
       {/* <Route path="/home/*" element={user ? <Homepage /> : <Navigate to="/admin" />} /> */}
     </Routes>
   );
 };
-=======
-    <Route path="/" element={ <Homepage />} />
-    <Route path="/student/*" element={ <StudentHomepage />} />
-    <Route path="/login" element={user ? <Navigate to="/home" /> : <Login />} />
-    <Route path="/Signup" element={user ? <Navigate to="/home" /> : <Signup />} />
-    {/* <Route path="/home/*" element={user ? <Homepage /> : <Navigate to="/admin" />} /> */}
->>>>>>> c47d8d5d0a8990cd8b71e1bb3b527dc11be15b34
 
 export default UserRoutes;

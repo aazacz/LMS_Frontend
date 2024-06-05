ae75c5ad1373896a8cd613f4cda4723adaaf5cdb
aazacz <113631218+aazacz@users.noreply.github.com>
2024-06-05 12:47:40 +0530

    added assignment page

--- src/pages/student/DiagnosisTestResult.jsx
+++ src/pages/student/DiagnosisTestResult.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import DoughnutChart from './DoughnutChart'
+
 
 const DiagnosisTestResult = () => {
-    console.log("difgrifgruifgwdruif")
+
+
     return (
 
-        <div className='w-screen h-screen p-20'>
+        <div className='Test w-screen h-screen overflow-y-scroll p-24 '>
 
 
             <div className='w-full h-[400px] rounded-[35px] bg-white border-[#0066DE]  border-2 flex flex-col justify-center items-center'>
@@ -26,23 +29,71 @@ const DiagnosisTestResult = () => {
             </div>
 
 
-            <div className='w-full'>
-              
-                <h1>Overall Analysis</h1>
+            <div className='w-full py-8 -200 mt-10'>
 
-                <div className='w-full px-8 rounded-md  border-[1px] border-[#0066DE] bg-white'>
-                <div>
-                    <h2> 10 Questions</h2>
-                    <h2> 9 Correct</h2>
-                    <h2> 9 Questions</h2>
-                    <h2> 1 Wrong</h2>
-                    <h2> 90% Accuracy</h2>
-                </div>
-            
-                </div>
+                <h1 className='font-poppins font-bold text-lg'>Overall Analysis</h1>
 
+                <div className='w-full px-4 rounded-[35px] bg-white border-[#0066DE]  border-2 py-10'>
+                    <div className='flex justify-around '>
+
+                        <div className='w-[170px] h-[170px] relative flex items-center justify-center'>
+
+                            <div className='w-full h-full  flex justify-center items-center'>
+                                <DoughnutChart />
+                            </div>
+
+                            <h1 className='font-poppins font-bold text-xs border  absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 '>10 Questions</h1>
+                        </div>
+
+                        <span className='flex items-center gap-x-2'>     <span className='w-6 h-6 rounded-full bg-[#444444]'></span>
+                            <h2> 10 Questions</h2>
+                        </span>
+
+                        <span className='flex items-center gap-x-2'>     <span className='w-6 h-6 rounded-full bg-[#67FE4E]'></span>
+                            <h2> 10 Questions</h2>
+                        </span>
+                        <span className='flex items-center gap-x-2'>     <span className='w-6 h-6 rounded-full bg-[#FE4E4E]'></span>
+                            <h2> 10 Questions</h2>
+                        </span>
+                        <span className='flex items-center gap-x-2'>     <span className='w-6 h-6 rounded-full bg-[#FB4EFE]'></span>
+                            <h2> 10 Questions</h2>
+                        </span>
+
+                    </div>
+
+                </div>
 
             </div>
+
+
+
+            <div className='w-full  mt-10'>
+
+                <div className='w-full p-8 rounded-[35px] bg-white border-[#0066DE]  border-2 py-10'>
+                    <h1 className='font-poppins font-semibold text-xl'>Explanation</h1>
+                    <div className='flex  mt-2' >
+                        <div className='flex items-center gap-x-5 '>
+
+                            <div className='w-8 h-8 bg-black rounded-full text-white font-semibold font-poppins flex justify-center items-center'>1</div>
+                            <h1 className='font-poppins font-semibold'>If Surya has 5 apples and surya ate 5 apples. How many apples does surya have now?</h1>
+                        </div>
+
+                    </div>
+
+                    <div className='w-full'>
+<div>
+    <span className='flex gap-x-2 items-center'> <div className='w-1 h-1 rounded-full bg-black'></div> Office ipsum you must be muted.</span> 
+    <span className='flex gap-x-2 items-center'> <div className='w-1 h-1 rounded-full bg-black'></div> Office ipsum you must be muted.</span> 
+    <span className='flex gap-x-2 items-center'> <div className='w-1 h-1 rounded-full bg-black'></div> Office ipsum you must be muted.</span> 
+    <span className='flex gap-x-2 items-center'> <div className='w-1 h-1 rounded-full bg-black'></div> Office ipsum you must be muted.</span> 
+</div>
+                    </div>
+
+                </div>
+
+            </div>
+
+
 
 
         </div>
--- src/pages/student/StudentHomepage.jsx
+++ src/pages/student/StudentHomepage.jsx
@@ -5,6 +5,10 @@ import DiagnosisTest from './DiagnosisTest';
 import UserNavbar from '../../components/User/UserNavbar';
 import ErrorPage from '../ErrorPage';
 import DiagnosisTestResult from './DiagnosisTestResult';
+import CourseList from '../../components/User/CourseList';
+import Coursedetails from '../../components/User/Coursedetails';
+import TutorListing from '../../components/User/TutorListing';
+import Assignments from './Assignments';
 
 const StudentHomepage = () => {
 
@@ -15,13 +19,13 @@ const StudentHomepage = () => {
         setIsSidebarOpen(!isSidebarOpen);
     };
     return (
-        <div className=" h-screen overflow-hidden">
+        <div className=" h-screen w-screen overflow-y-scroll flex flex-col  items-center Test">
 
             <div className="sticky top-0 z-10">
-                <UserNavbar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
+                    <UserNavbar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
             </div>
 
-            <div className="flex flex-col flex-grow">
+            <div className="flex flex-col   ">
 
 
                 {/* Main Content */}
@@ -30,6 +34,11 @@ const StudentHomepage = () => {
                     <Routes>
                         <Route path="/*" element={<DiagnosisTest />} />
                         <Route path="/diagnosistestresult" element={<DiagnosisTestResult />} />
+                        <Route path="/courses" element={<CourseList />} />
+                        <Route path="/assignments" element={<Assignments />} />
+                        <Route path="/coursedetails" element={<Coursedetails />} />
+                        <Route path="/tutors" element={<TutorListing />} />
+
                         {/* <Route path="/sdiagnosistest" element={<DiagnosisTest />} /> */}
                         {/* <Route path="/courses/addcourse" element={<AddCourse />} />
                         <Route path="/courses/:courseId" element={<Coursedetails />} />
@@ -42,6 +51,7 @@ const StudentHomepage = () => {
                         <Route path="/tutors/:tutorId" element={<TutorDetails />} />
                         <Route path="/tutors" element={<TutorListing />} />
                         <Route path="/library" element={<Library />} /> */}
+
                         <Route path="*" element={<ErrorPage />} />
                     </Routes>
                 </div>

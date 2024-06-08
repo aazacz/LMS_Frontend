6b8664660b934250ae9300991997190745c5029d
hibahfathima15 <hibahfathima15@gmail.com>
2024-06-08 16:38:36 +0530

    classes today today

--- src/pages/student/HomePage.jsx
+++ src/pages/student/HomePage.jsx
@@ -18,27 +18,6 @@ const Homepage = () => {
       </div>
 
 
-<<<<<<< HEAD
-                {/* Main Content */}
-
-                <div className="flex-grow overflow-auto ">
-                    <Routes>
-                        <Route path="/*" element={<AnimationScreen/>} />
-                        {/* <Route path="/sdiagnosistest" element={<DiagnosisTest />} /> */}
-                        {/* <Route path="/courses/addcourse" element={<AddCourse />} />
-                        <Route path="/courses/:courseId" element={<Coursedetails />} />
-                        <Route path="/coursestructure" element={<CourseStructure />} />
-                        <Route path="/students/:studentId" element={<StudentDetail />} />
-                        <Route path="/students" element={<StudentList />} />
-                        <Route path="/tutors/addtutor" element={<AddTutor />} />
-                        <Route path="/diagnosistest/addiagnosistest" element={<Addiagnosistest />} />
-                        <Route path="/diagnosistest" element={<DiagnosisTest />} />
-                        <Route path="/tutors/:tutorId" element={<TutorDetails />} />
-                        <Route path="/tutors" element={<TutorListing />} />
-                        <Route path="/library" element={<Library />} /> */}
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-=======
 
       <div className="maincontainer h-screen w-full flex flex-col justify-center items-center animationapart overflow-x-hidden">
         <div className="flex flex-col flex-grow w-full overflow-y-auto Test">
@@ -49,7 +28,6 @@ const Homepage = () => {
               <Route path="*" element={<ErrorPage />} />
             </Routes>
           </div>
->>>>>>> e0ef2f7a3015fdb93c26ddb02e1b28d8339114b7
         </div>
       </div>
     </div>

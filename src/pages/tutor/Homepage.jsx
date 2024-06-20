a22d1bf01faed6139c41608dbf2652e481f3527c
hibahfathima15 <hibahfathima15@gmail.com>
2024-06-20 10:37:21 +0530

    created grading, content page, library

--- src/pages/tutor/Homepage.jsx
+++ src/pages/tutor/Homepage.jsx
@@ -5,20 +5,29 @@ import Sidebar from "../../components/tutor/Sidebar";
 import Dashboard from "../../components/tutor/Dashboard";
 import Content from "../../components/tutor/Content";
 import QuestionBank from "../../components/tutor/QuestionBank";
-import Students from "../../components/tutor/Students";
-import Grading from "../../components/tutor/Grading";
 import Insights from "../../components/tutor/Insights";
 import Settings from "../../components/tutor/Settings";
 import ErrorPage from "../ErrorPage";
+import Coursedetails from "../../components/tutor/Coursedetails";
+import Assignments from "../../components/tutor/Assignment/Assignments";
+import NewCourse from "../../components/tutor/NewCourse/NewCourse";
+import Material from "../../components/tutor/Material/Material";
+import AddTest from "../../components/tutor/AddTest";
+import Students from "../../components/tutor/Students";
+import StudentDetail from "../../components/tutor/StudentDetail/StudentDetail";
+import Grading from "../../components/tutor/Grading/Grading";
+import Library from "../../components/tutor/Library";
+import GradingAssignment from "../../components/tutor/GradingAssignment";
+import MarksGrading from "../../components/tutor/MarksGrading";
 
 const Homepage = () => {
-  // const divRef = useRef(null);
+  const divRef = useRef(null);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
-  // const [Ref, SetRef] = useState()
+  const [Ref, SetRef] = useState();
 
   return (
     <>
@@ -34,9 +43,30 @@ const Homepage = () => {
           <Routes>
             <Route path="/*" element={<Dashboard />} />
             <Route path="/content" element={<Content />} />
+            <Route
+              path="/courses/:courseId"
+              element={<Coursedetails height={Ref} />}
+            />
+            <Route path="/content/newcourse" element={<NewCourse />} />
+            <Route
+              path="/content/:courseId/newassignment"
+              element={<Assignments />}
+            />
+            <Route path="/content/addtest" element={<AddTest />} />
+            <Route path="/content/material" element={<Material />} />
             <Route path="/questionbank" element={<QuestionBank />} />
+            <Route path="/library" element={<Library />} />
             <Route path="/students" element={<Students />} />
+            <Route path="/students/:studentId" element={<StudentDetail />} />
             <Route path="/grading" element={<Grading />} />
+            <Route
+              path="/grading/assignment/:assignmentId"
+              element={<GradingAssignment />}
+            />
+            <Route
+              path="/grading/assignment/:assignmentId/marks"
+              element={<MarksGrading />}
+            />
             <Route path="/insights" element={<Insights />} />
             <Route path="/settings" element={<Settings />} />
 

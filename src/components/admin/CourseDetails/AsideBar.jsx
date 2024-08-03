a92aeca2e3857bd6e9d6b0f70802b999a650d669
aazacz <113631218+aazacz@users.noreply.github.com>
2024-08-03 10:52:38 +0530

    changes in the conflict done

--- src/components/admin/CourseDetails/AsideBar.jsx
+++ src/components/admin/CourseDetails/AsideBar.jsx
@@ -69,138 +69,6 @@ const Asidebar = ({ course, setTutorModal, setStudentModal }) => {
           cancelButtonColor: "#878ca7",
           confirmButtonText: "Yes, delete it!",
         }).then((result) => {
-            if (result.isConfirmed) {
-                Swal.fire({
-                    title: 'Are you absolutely sure?',
-                    text: "You won't be able to revert this!",
-                    icon: 'warning',
-                    timer: 2000,
-                    timerProgressBar: true,
-                    showCancelButton: true,
-                    confirmButtonColor: '#eb5048',
-                    cancelButtonColor: '#878ca7',
-                    confirmButtonText: 'Yes, delete it!',
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        deleteHandler(courseId)
-                    }
-                })
-            }
-        })
-    }
-
-    
-
-    const handleEditCourse = () => {
-        Swal.fire({
-            title: `Are you sure you want to edit this course?`,
-            text: "You won't be able to revert this!",
-             showCancelButton: true,
-            confirmButtonColor: '#eb5048',
-            cancelButtonColor: '#878ca7',
-            confirmButtonText: 'Edit Course!',
-        }).then((result) => {
-            if (result.isConfirmed) {
-       
-                navigate(`/admin/home/courses/editcourse/${courseId}`)
-                
-            }
-        })
-    }
-
-    return (
-        <div className="relative bg-slate-200 lg:w-[30%] w-full   flex flex-col">
-
-            {/* adding student and tutor */}
-            <div className='w-full  flex justify-evenly h-max p-4 '>
-
-                <div
-                    onClick={() => {           setTutorModal(true)}
-                               }
-                    data-tooltip-id="assigntutor"
-                    data-tooltip-content="Assign Tutor"
-                    className='rounded-full border-[3px] text-gray-700 hover:text-gray-900  border-gray-700 hover:border-gray-900 transition-all duration-200 bg-white  w-14 h-14 flex justify-center items-center  ' >
-                    +<FaChalkboardTeacher className='text-2xl' />
-
-                    <Tooltip
-                        id="assigntutor"
-                        place="bottom"
-                        type="dark"
-                        effect="solid"
-                    />
-                </div>
-                <div
-                    onClick={() => {
-                        setStudentModal(true)}
-                    }
-                    data-tooltip-id="assignstudent"
-                    data-tooltip-content="Assign Student"
-                    className='rounded-full border-[3px] text-gray-700 hover:text-gray-900  border-gray-700 hover:border-gray-900 transition-all duration-200 bg-white  w-14 h-14 flex justify-center items-center  ' >
-
-                    +<PiStudentBold className='text-2xl' />
-
-                    <Tooltip
-                        id="assignstudent"
-                        place="bottom"
-                        type="dark"
-                        effect="solid"
-                    />
-                </div>
-
-            </div>
-
-
-            <div className="px-6">
-                <h1 className="font-plusjakartasans font-bold">Modules List</h1>
-
-                <div className="bg-white rounded-lg flex flex-col mt-5 p-5 items-center">
-                    <h1 className="font-plusjakartasans font-bold line-clamp-2">
-                        Introduction Basic SAT & DSAT
-                    </h1>
-
-                    {modules.map((value, index) => (
-                        <div
-                            key={index}
-                            className="flex w-full justify-between items-center py-5"
-                        >
-                            <div className="flex gap-x-3 items-center w-[65%]">
-                                <div>
-                                    <div className="w-6 h-6 bg-[#C75625] text-white rounded-[5px] text-sm flex justify-center items-center">
-                                        {index + 1}
-                                    </div>
-                                </div>
-
-                                <h1 className="text-orange-600 text-[12px] line-clamp-1">
-                                    {value.moduleName}
-                                </h1>
-                            </div>
-
-                            <h1 className="w-[35%] text-right text-xs text-gray-400">
-                                {value.sessions ? value.sessions.length : '0'}{' '}
-                                Sessions
-                            </h1>
-                        </div>
-                    ))}
-                </div>
-            </div>
-
-            <div className="absolute bottom-2 w-full grid grid-flow-row grid-cols-2  gap-x-4 mb-4 px-4">
-
-                <div
-                    className="cursor-pointer  h-8 rounded-xl flex justify-center items-center text-base font-semibold font-poppins border-[1px] text-blue-700 border-blue-600 bg-opacity-30 bg-blue-500"
-                    onClick={()=>handleEditCourse()}
-                >
-                    <h1>Edit</h1> 
-                </div>
-
-                <div
-                    className="cursor-pointer  h-8 rounded-xl flex justify-center items-center text-base font-semibold font-poppins border-[1px] text-red-700 border-red-600 bg-opacity-30 bg-red-500"
-                    onClick={handleDeleteCourse}
-                >
-                    <h1>Delete Course</h1>
-                </div>
-
-            </div>
           if (result.isConfirmed) {
             deleteHandler(courseId);
           }
@@ -209,36 +77,42 @@ const Asidebar = ({ course, setTutorModal, setStudentModal }) => {
     });
   };
 
+  const handleEditCourse = () => {
+    Swal.fire({
+      title: `Are you sure you want to edit this course?`,
+      text: "You won't be able to revert this!",
+      showCancelButton: true,
+      confirmButtonColor: "#eb5048",
+      cancelButtonColor: "#878ca7",
+      confirmButtonText: "Edit Course!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        navigate(`/admin/home/courses/editcourse/${courseId}`);
+      }
+    });
+  };
+
   return (
-    <div className="relative bg-slate-200 lg:w-[30%] w-full   flex flex-col">
-      {/* adding student and tutor */}
-      <div className="w-full  flex justify-evenly h-max p-4 ">
+    <div className="relative bg-slate-200 lg:w-[30%] w-full flex flex-col">
+      {/* Adding student and tutor */}
+      <div className="w-full flex justify-evenly h-max p-4">
         <div
-          onClick={() => {
-            setTutorModal(true);
-          }}
+          onClick={() => setTutorModal(true)}
           data-tooltip-id="assigntutor"
           data-tooltip-content="Assign Tutor"
-          className="rounded-full border-[3px] text-gray-700 hover:text-gray-900  border-gray-700 hover:border-gray-900 transition-all duration-200 bg-white  w-14 h-14 flex justify-center items-center  "
+          className="rounded-full border-[3px] text-gray-700 hover:text-gray-900 border-gray-700 hover:border-gray-900 transition-all duration-200 bg-white w-14 h-14 flex justify-center items-center"
         >
           +<FaChalkboardTeacher className="text-2xl" />
           <Tooltip id="assigntutor" place="bottom" type="dark" effect="solid" />
         </div>
         <div
-          onClick={() => {
-            setStudentModal(true);
-          }}
+          onClick={() => setStudentModal(true)}
           data-tooltip-id="assignstudent"
           data-tooltip-content="Assign Student"
-          className="rounded-full border-[3px] text-gray-700 hover:text-gray-900  border-gray-700 hover:border-gray-900 transition-all duration-200 bg-white  w-14 h-14 flex justify-center items-center  "
+          className="rounded-full border-[3px] text-gray-700 hover:text-gray-900 border-gray-700 hover:border-gray-900 transition-all duration-200 bg-white w-14 h-14 flex justify-center items-center"
         >
           +<PiStudentBold className="text-2xl" />
-          <Tooltip
-            id="assignstudent"
-            place="bottom"
-            type="dark"
-            effect="solid"
-          />
+          <Tooltip id="assignstudent" place="bottom" type="dark" effect="solid" />
         </div>
       </div>
 
@@ -251,10 +125,7 @@ const Asidebar = ({ course, setTutorModal, setStudentModal }) => {
           </h1>
 
           {modules.map((value, index) => (
-            <div
-              key={index}
-              className="flex w-full justify-between items-center py-5"
-            >
+            <div key={index} className="flex w-full justify-between items-center py-5">
               <div className="flex gap-x-3 items-center w-[65%]">
                 <div>
                   <div className="w-6 h-6 bg-[#C75625] text-white rounded-[5px] text-sm flex justify-center items-center">
@@ -270,27 +141,25 @@ const Asidebar = ({ course, setTutorModal, setStudentModal }) => {
               <h1 className="w-[35%] text-right text-xs text-gray-400">
                 {value.sessions ? value.sessions.length : "0"} Sessions
               </h1>
-              
             </div>
-            
           ))}
-          <div className="w-full grid grid-flow-row grid-cols-2  gap-x-4 mb-4 px-4">
-          <div
-            className="cursor-pointer  h-max rounded-xl flex justify-center items-center text-base font-semibold font-poppins border-[1px] text-blue-700 border-blue-600 bg-opacity-30 bg-blue-500"
-            onClick={handleDeleteCourse}
-          >
-            <h1 className="text-sm md:text-base">Edit</h1>
-          </div>
 
-          <div
-            className="cursor-pointer  h-max rounded-xl flex justify-center items-center text-base font-semibold font-poppins border-[1px] text-red-700 border-red-600 bg-opacity-30 bg-red-500"
-            onClick={handleDeleteCourse}
-          >
-            <h1 className="text-sm md:text-base">Delete</h1>
+          <div className="w-full grid grid-flow-row grid-cols-2 gap-x-4 mb-4 px-4">
+            <div
+              className="cursor-pointer h-max rounded-xl flex justify-center items-center text-base font-semibold font-poppins border-[1px] text-blue-700 border-blue-600 bg-opacity-30 bg-blue-500"
+              onClick={handleEditCourse}
+            >
+              <h1 className="text-sm md:text-base">Edit</h1>
+            </div>
+
+            <div
+              className="cursor-pointer h-max rounded-xl flex justify-center items-center text-base font-semibold font-poppins border-[1px] text-red-700 border-red-600 bg-opacity-30 bg-red-500"
+              onClick={handleDeleteCourse}
+            >
+              <h1 className="text-sm md:text-base">Delete</h1>
+            </div>
           </div>
         </div>
-        </div>
-        
       </div>
     </div>
   );
